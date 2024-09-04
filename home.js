deb4c6600546b6ed149c58b6ea426ced266c03a0
msetitech <drmseti@gmail.com>
2024-09-04 10:01:45 +0300

    Add to cart functionalit added Day 02

--- home.js
+++ home.js
@@ -1,8 +1,66 @@
 "use strict";
 
-const cart = document.querySelector(".cart");
-cart.addEventListener("click", () => {
+const cartiew = document.querySelector(".cart");
+cartiew.addEventListener("click", () => {
   const activeCart = document
     .querySelector(".cartSection")
     .classList.toggle("activeCart");
 });
+
+let cart = [];
+const handleAddRoCArt = () => {
+  const addCartBtn = document.querySelectorAll(".addToCartBtn");
+  addCartBtn.forEach((button) => {
+    button.addEventListener("click", () => {
+      let productCard = button.closest(".productItem");
+      const productImage = productCard.querySelector(".prodImage").src;
+      const productTitle = productCard.querySelector(".productTitle").innerHTML;
+      const productDescription = productCard.querySelector(
+        ".productDescription"
+      ).innerHTML;
+      const productPrice = productCard.querySelector(".productPrice").innerHTML;
+
+      let product = {
+        productImage,
+        productTitle,
+        productDescription,
+        productPrice,
+      };
+
+      cart.push(product);
+      handlecartCount(cart);
+      handleCartDisplay(cart);
+    });
+  });
+};
+
+handleAddRoCArt();
+
+const handlecartCount = (cart) => {
+  const countCart = document.querySelector(".number");
+  countCart.textContent = cart.length;
+};
+
+const handleCartDisplay = (cart) => {
+  const cardsection = document.querySelector(".cartSection");
+
+  cardsection.innerHTML = "";
+
+  cart.forEach((cartItem) => {
+    const CartContent = `
+        <div class="cartProduct">
+          <div class="image">
+            <img src="${cartItem.productImage}" alt="" />
+          </div>
+          <div class="details">
+            <h1>${cartItem.productTitle}</h1>
+            <p>${cartItem.productDescription}</p>
+            <div class="price">
+              <h1>${cartItem.productPrice}</h1>
+            </div>
+          </div>
+        </div>
+      `;
+    cardsection.insertAdjacentHTML("beforeend", CartContent);
+  });
+};
